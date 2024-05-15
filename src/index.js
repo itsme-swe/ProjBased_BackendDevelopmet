29985a4703185226512dc9c921508b6195492474
Harsh Mehra <me.harshmehra07@gmail.com>
2024-05-15 16:29:45 +0530

    update

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import dotenv from "dotenv"
-import connectDB from "./db/index.js";
+
+import connectDB from "./db/index.js";      // importing db connection function from db folder
 
 dotenv.config({
     path: './env'
 })
 
-connectDB()
+connectDB()     // Invoking connectDB() function in index.js file 
