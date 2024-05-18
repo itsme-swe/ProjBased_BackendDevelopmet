c63a746b1b3ee0dac422c248f34d0e3e1efb4047
Harsh Mehra <me.harshmehra07@gmail.com>
2024-05-18 16:13:38 +0530

    update

--- src/app.js
+++ src/app.js
@@ -10,13 +10,13 @@ app.use(cors({
     credentials: true
 }))
 
-app.use(express.json({limit: "16kb"}))                              // configuring .json format setting limit to accept json 
+app.use(express.json({limit: "16kb"}))                              // configuring .json() middleware and setting limit to accept json format files
 
-app.use(express.urlencoded({extended: true, limit: "16kb"}))       // configuring for URL's 
+app.use(express.urlencoded({extended: true, limit: "16kb"}))       // configuring for URL middleware to accept data coming from URL
 
 app.use(express.static("public"))       // Use to keep assets like pdf files, images etc in our local server so we create one folder by the name of public
 
 app.use(cookieParser())
 
 
-export {app}
+export { app }
---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,28 @@
+//◽Method 1 :- Promise asyncHandler function
+
+const asyncHandler = (reqHandler) => {
+    (req, res, next) => {
+        Promise.resolve(reqHandler(req, res, next))
+        .catch((err) => next(err))
+    }
+}
+
+export {asyncHandler}
+
+
+
+
+//◽Method 2 :- try() & catch() asyncHandler function
+
+/*
+const asyncHandler = (fn) => async (err, req, res, next) => {
+    try {
+        await fn(err, req, res, next)
+    } catch (error) {
+        res.status(err.code || 500).json({
+            success: false,
+            message: err.message
+        })
+    }
+}
+*/
