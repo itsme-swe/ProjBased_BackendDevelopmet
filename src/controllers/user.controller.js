054cfa3e28d3344caf3928c2b72a3723f6429b98
Harsh Mehra <me.harshmehra07@gmail.com>
2024-05-23 22:16:54 +0530

    update

---
+++ src/controllers/user.controller.js
@@ -0,0 +1,9 @@
+import { asyncHandler } from "../utils/asyncHandler.js"
+
+// Here asyncHandler() is our "Higher order function" and higher order function takes another function as parameter
+const registerUser = asyncHandler( async (req, res) => {
+    res.status(200).json({
+        message: "OK"
+    })
+})
+
