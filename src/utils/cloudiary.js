d9d52046530c8075b0c69859ae61bac7c3cc1bcb
Harsh Mehra <me.harshmehra07@gmail.com>
2024-05-23 07:23:23 +0530

    update

--- src/utils/cloudiary.js
+++ src/utils/cloudiary.js
@@ -1,5 +1,6 @@
 // Setting up cloudiary to manage our files
 import { v2 as cloudinary} from "cloudinary";
+import { error } from "console";
 
 import fs from "fs"
 
@@ -9,3 +10,26 @@ import fs from "fs"
     api_key: process.env.CLOUDINARY_API_KEY, 
     api_secret: process.env.CLOUDINARY_API_SECERET // Click 'View Credentials' below to copy your API secret
 });
+
+const uploadOnCloud = async (localFilePath) => {
+    try {
+        if (!localFilePath) throw error("Not able to find the Path !!")    // Here instead of throwing an error we can also "return null" and this condition says {agar "localFilePath" nhi ha toh 1 error bhejo}    
+        
+        // Now upload the file on cloudinary
+        const response = await cloudinary.uploader.upload(localFilePath,
+            {
+                resource_type: "auto"
+            })
+
+        // Now if file is uploaded successfully then
+        console.log("File has been uploaded on cloudinary", response.url);
+        return response     // This will return whole response to user 
+        
+    } catch (error) {
+        // Now removing file from our local server
+        fs.unlinkSync(localFilePath)    // Removed the locally saved temp file as upload operation got failed
+        return null
+    }
+} 
+
+export { uploadOnCloud }
